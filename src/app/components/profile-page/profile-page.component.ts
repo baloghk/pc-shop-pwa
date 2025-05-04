0828cad461443228d11b28e23a121ab8490e4d3a
Balogh Károly <h252910@stud.u-szeged.hu>
2025-05-05 01:33:43 +0200

    profile page frontend

--- src/app/components/profile-page/profile-page.component.ts
+++ src/app/components/profile-page/profile-page.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-profile-page',
-  imports: [],
+  imports: [MatButton],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss'
 })
 export class ProfilePageComponent {
+
+  username: string = 'johndoe';
+  email: string = 'johndoe3@gmail.com';
+  name: string = 'John Doe';
 
 }
