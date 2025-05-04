eb2f0f399b8303bf4bae36ee7ece985bdea78903
Balogh Károly <h252910@stud.u-szeged.hu>
2025-05-04 20:50:24 +0200

    login page frontend, registration fix ,navbar fix

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [RouterLink, MatFormFieldModule, MatInputModule, MatButton, MatIcon],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
