bcc51d44124848cbde2d5290387f9f17afb5459d
Balogh Károly <h252910@stud.u-szeged.hu>
2025-05-15 14:19:36 +0200

    angular downgrade, firebase package

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
-          })]
+          }), provideFirebaseApp(() => initializeApp({ projectId: "pc-shop-pwa", appId: "1:662803522709:web:bbfb597da28cf04699679c", storageBucket: "pc-shop-pwa.firebasestorage.app", apiKey: "AIzaSyDSuuazWcB4mJUQIwJ1At83S9-8w5GM3zY", authDomain: "pc-shop-pwa.firebaseapp.com", messagingSenderId: "662803522709" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
