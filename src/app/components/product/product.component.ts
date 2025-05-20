670de98a47846ebe07d81e55cea1152feb0894da
Balogh Károly <balogh2k4@gmail.com>
2025-05-20 21:52:42 +0200

    filtering implementation

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatCard } from '@angular/material/card';
 import { MatCardActions } from '@angular/material/card';
 import { MatCardTitle } from '@angular/material/card';
@@ -9,6 +9,8 @@ import { NgFor } from '@angular/common';
 import { CurrencyPipe } from '../../shared/currency-pipe/currency.pipe';
 import { Router } from '@angular/router';
 import { ProductsFirebaseService } from '../../services/firebase/products/products-firebase.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product',
@@ -25,24 +27,40 @@ import { ProductsFirebaseService } from '../../services/firebase/products/produc
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, OnDestroy {
   constructor(
     private router: Router,
+    private activatedRoute: ActivatedRoute,
     private productsService: ProductsFirebaseService
   ) {}
 
+  private queryParamSubscription: Subscription | undefined;
   Products: any[] = [];
 
   ngOnInit() {
-    this.productsService.getProductsCollection().subscribe(
-      (data) => {
-        this.Products = data;
-        console.log('Termékek:', this.Products);
-      },
-      (error) => {
-        console.error('Hiba a termékek betöltésekor:', error);
+    this.queryParamSubscription = this.activatedRoute.queryParams.subscribe(
+      (params) => {
+        const category = params['category'] || '';
+        this.productsService.getProductsByCategory(category).subscribe({
+          next: (data) => {
+            this.Products = data;
+            console.log('Termékek:', this.Products);
+          },
+          error: (error) => {
+            console.error('Hiba a termékek betöltésekor:', error);
+          },
+          complete: () => {
+            console.log('Lekérés befejezve.');
+          },
+        });
       }
     );
+  }
+
+  ngOnDestroy(): void {
+    if (this.queryParamSubscription) {
+      this.queryParamSubscription.unsubscribe();
+    }
   }
 
   goToProduct(product: Product): void {
--- src/app/services/firebase/products/products-firebase.service.ts
+++ src/app/services/firebase/products/products-firebase.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import {
+  Firestore,
+  collection,
+  collectionData,
+  query,
+  where,
+} from '@angular/fire/firestore';
+import { filter, from, map, mergeMap, Observable, tap, toArray } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +17,25 @@ export class ProductsFirebaseService {
   getProductsCollection(): Observable<any[]> {
     const productsRef = collection(this.firestore, 'products');
     return collectionData(productsRef, { idField: 'id' }) as Observable<any[]>;
+  }
+
+  getProductsByCategory(category?: string): Observable<any[]> {
+    const productsRef = collection(this.firestore, 'products');
+
+    if (!category) {
+      return collectionData(productsRef, { idField: 'id' }) as Observable<
+        any[]
+      >;
+    }
+
+    return collectionData(productsRef, { idField: 'id' }).pipe(
+      map((items) =>
+        items.filter(
+          (item) =>
+            item['category'] &&
+            item['category'].toLowerCase() === category.toLowerCase()
+        )
+      )
+    ) as Observable<any[]>;
   }
 }
